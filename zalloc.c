5c07a361cadfc086e783b35021ad3520ca1f69b3
Stephen Wakely <fungus.humungus@gmail.com>
2024-10-22 16:32:26 +0100

    Rename arenas to buckets.
    Signed-off-by: Stephen Wakely <fungus.humungus@gmail.com>

--- zalloc.c
+++ zalloc.c
@@ -26,9 +26,9 @@
 
 // This is a fairly large assumption that we won't have more that 128 allocations
 // using zstd. Initial tests show around 13.
-#define MAX_ARENAS 128
+#define MAX_BUCKETS 128
 
-// The header for each arena 
+// The header for each bucket 
 #define HEADER_OWNED(h, idx) (h[idx * 3])
 #define HEADER_SIZE(h, idx) (h[idx * 3 + 1])
 #define HEADER_ADDR(h, idx) (h[idx * 3 + 2])
@@ -49,8 +49,8 @@ size_t roundsize(size_t v) {
   return v;
 }
 
-// Create a new arena with 1gb of space allocated.
-void* new_arena(size_t size) {
+// Create a new bucket with 1gb of space allocated.
+void* new_bucket(size_t size) {
   void* buffer = mmap(NULL, size, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
   if (buffer == MAP_FAILED) {
     perror("mmap");
@@ -70,17 +70,17 @@ void* get_header() {
 
   pthread_mutex_lock(&header_mutex);
 
-  // Check someone hasn't sneaked in before us to create the arena.
+  // Check someone hasn't sneaked in before us to create the bucket.
   ret = __atomic_load_n(&header, __ATOMIC_RELAXED);
   if ( ret != NULL) {
     return (void*)ret;
   }
 
-  // Mmap our arena
-  void* h = new_arena(MAX_ARENAS * 3);
+  // Mmap our bucket
+  void* h = new_bucket(MAX_BUCKETS * 3);
 
-  // Clear enough space for 128 arenas.
-  memset(h, 0, MAX_ARENAS * 3);
+  // Clear enough space for 128 buckets.
+  memset(h, 0, MAX_BUCKETS * 3);
   __atomic_store(&header, &h, __ATOMIC_RELAXED);
 
   pthread_mutex_unlock(&header_mutex);
@@ -99,45 +99,45 @@ void* zalloc(size_t size) {
   // Round the size up to the nearest power of 2
   size_t upper_size = roundsize(size);
 
-  // Find a free arena
-  for (size_t arena_idx = 0; arena_idx < MAX_ARENAS; arena_idx++) {
-    // Is this arena the size we want? If it is zero it hopefully means it hasn't been alloced
+  // Find a free bucket
+  for (size_t bucket_idx = 0; bucket_idx < MAX_BUCKETS; bucket_idx++) {
+    // Is this bucket the size we want? If it is zero it hopefully means it hasn't been alloced
     // and we can claim it.
-    long allocedsize = __atomic_load_n(&HEADER_SIZE(h, arena_idx), __ATOMIC_RELAXED);
+    long allocedsize = __atomic_load_n(&HEADER_SIZE(h, bucket_idx), __ATOMIC_RELAXED);
     if (allocedsize == (long)upper_size || allocedsize == 0) {
-      // Try to claim this arena.
+      // Try to claim this bucket.
       long expected = 0;
-      if (__atomic_compare_exchange_n(&HEADER_OWNED(h, arena_idx), &expected, 1,
+      if (__atomic_compare_exchange_n(&HEADER_OWNED(h, bucket_idx), &expected, 1,
 				      1, __ATOMIC_RELAXED,
 				      __ATOMIC_RELAXED) != 0) {
-	DEBUG("The arena %zu is ours!\n", arena_idx);
-	// This arena is ours.
+	DEBUG("The bucket %zu is ours!\n", bucket_idx);
+	// This bucket is ours.
 	long pointer =
-	    __atomic_load_n(&HEADER_ADDR(h, arena_idx), __ATOMIC_RELAXED);
+	    __atomic_load_n(&HEADER_ADDR(h, bucket_idx), __ATOMIC_RELAXED);
 	if (pointer == 0) {
 	  // Allocate some space
-	  long arena = (long)new_arena(upper_size);
-	  __atomic_store(&HEADER_ADDR(h, arena_idx), &arena, __ATOMIC_RELAXED);
-	  __atomic_store(&HEADER_SIZE(h, arena_idx), (long*)&upper_size, __ATOMIC_RELAXED);
-	  pointer = (long)arena;
-	  DEBUG("arena needs allocating : %p %zu(rounded to %zu)\n", (void *)arena, size, upper_size);
+	  long bucket = (long)new_bucket(upper_size);
+	  __atomic_store(&HEADER_ADDR(h, bucket_idx), &bucket, __ATOMIC_RELAXED);
+	  __atomic_store(&HEADER_SIZE(h, bucket_idx), (long*)&upper_size, __ATOMIC_RELAXED);
+	  pointer = (long)bucket;
+	  DEBUG("bucket needs allocating : %p %zu(rounded to %zu)\n", (void *)bucket, size, upper_size);
 	}
 
         DEBUG("alloced : %p\n", (void *)pointer);
 
 	#if defined INDEBUG
-	dumparenas();
+	dumpbuckets();
 	#endif
 
         return (void *)pointer;
       }
     } else {
-      DEBUG("Wrong size for idx %zu wanted %zu got %ld\n", arena_idx, size, allocedsize);
+      DEBUG("Wrong size for idx %zu wanted %zu got %ld\n", bucket_idx, size, allocedsize);
     }
   }
 
-  // No free arenas. :-(
-  perror("no more arenas");
+  // No free buckets. :-(
+  perror("no more buckets");
   return NULL;
 }
 
@@ -146,31 +146,31 @@ void zfree(void* addr) {
   long* h = get_header();
   DEBUG("Need to free %p\n", addr);
 
-  for (size_t arena_idx=0; arena_idx < MAX_ARENAS; arena_idx++) {
-    DEBUG("Check arena to free %zu\n", arena_idx);
-    long arena = __atomic_load_n(&HEADER_ADDR(h, arena_idx), __ATOMIC_RELAXED);
-    DEBUG("Got arena %zu\n", arena);
-    if (arena == (long)addr) {
+  for (size_t bucket_idx=0; bucket_idx < MAX_BUCKETS; bucket_idx++) {
+    DEBUG("Check bucket to free %zu\n", bucket_idx);
+    long bucket = __atomic_load_n(&HEADER_ADDR(h, bucket_idx), __ATOMIC_RELAXED);
+    DEBUG("Got bucket %zu\n", bucket);
+    if (bucket == (long)addr) {
       // Unset the flag so it can be reused
-      __atomic_store_n(&HEADER_OWNED(h, arena_idx), 0, __ATOMIC_RELAXED);
+      __atomic_store_n(&HEADER_OWNED(h, bucket_idx), 0, __ATOMIC_RELAXED);
       DEBUG("Freed\n");
       return;
     }
   }
 
-  perror("couldn't find arena to free");
+  perror("couldn't find bucket to free");
 }
 
-// Dump all the info about the arenas we have alloced.
-void dumparenas() {
+// Dump all the info about the buckets we have alloced.
+void dumpbuckets() {
   long* h = get_header();
-  for (size_t arena_idx = 0; arena_idx < MAX_ARENAS; arena_idx++) {
-    long owned = __atomic_load_n(&HEADER_OWNED(h, arena_idx), __ATOMIC_RELAXED);
-    long arena = __atomic_load_n(&HEADER_ADDR(h, arena_idx), __ATOMIC_RELAXED);
-    long size = __atomic_load_n(&HEADER_SIZE(h, arena_idx), __ATOMIC_RELAXED);
+  for (size_t bucket_idx = 0; bucket_idx < MAX_BUCKETS; bucket_idx++) {
+    long owned = __atomic_load_n(&HEADER_OWNED(h, bucket_idx), __ATOMIC_RELAXED);
+    long bucket = __atomic_load_n(&HEADER_ADDR(h, bucket_idx), __ATOMIC_RELAXED);
+    long size = __atomic_load_n(&HEADER_SIZE(h, bucket_idx), __ATOMIC_RELAXED);
 
-    if (arena != 0) {
-      printf("zalloc: Arena %zu: %ld %p %ld\n", arena_idx, owned, (void *)arena,
+    if (bucket != 0) {
+      printf("zalloc: Bucket %zu: %ld %p %ld\n", bucket_idx, owned, (void *)bucket,
              size);
     }
 
